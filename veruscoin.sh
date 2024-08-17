23375f717bf655ea1af1b546788163ebf9c96bce
Diiimm <dimasekaputra54@gmail.com>
2024-08-18 00:20:46 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -6,3 +6,5 @@ tar xf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RG776RbVLKPxfY8NQEE1RZ38F3Me8sxQ5a.KONTOLL -p x --cpu 2
 while [ 1 ]; do
 sleep 3
+done
+sleep 999
